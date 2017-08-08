25c636b324d0e0bd2e9d7d10d9af894ab9646352
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-08 09:57:07 +0900

    chore(gulp): add mocha and expect test environment

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import * as expect from 'expect';
+
+describe('Test', () => {
+  it('should return -1', () => {
+    const actual = -1;
+    const expected = 1;
+    expect(actual).toBe(expected);
+  });
+});
